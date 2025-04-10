4d50d0baa31f15bd20ee12214c2a8d6b90161440
Gustavo Gonçalves <gugagon19@gmail.com>
2025-04-10 14:42:41 -0300

    0.1.0

---
+++ prisma/migrations/20250410151849_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "Habit" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Habit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "HabitEntry" (
+    "id" TEXT NOT NULL,
+    "habitId" TEXT NOT NULL,
+    "date" DATE NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "HabitEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Habit_userId_idx" ON "Habit"("userId");
+
+-- CreateIndex
+CREATE INDEX "HabitEntry_userId_idx" ON "HabitEntry"("userId");
+
+-- CreateIndex
+CREATE INDEX "HabitEntry_date_idx" ON "HabitEntry"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "HabitEntry_habitId_date_key" ON "HabitEntry"("habitId", "date");
+
+-- AddForeignKey
+ALTER TABLE "HabitEntry" ADD CONSTRAINT "HabitEntry_habitId_fkey" FOREIGN KEY ("habitId") REFERENCES "Habit"("id") ON DELETE CASCADE ON UPDATE CASCADE;
